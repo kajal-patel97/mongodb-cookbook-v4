b7e0fd4ab7b95d6bddb86bdb0356ac83dc7ccd7d
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2020-01-15 15:54:37 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# mongodb_cookbook_v4
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: mongodb_cookbook_v4
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
